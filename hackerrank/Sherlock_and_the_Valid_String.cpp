90550bc99e52827a479dfacec5a66e1a7af57a23
Sandro Martínez <virgosmp.mx@gmail.com>
2025-05-17 00:54:58 -0600

    refactor isValid function for clarity and efficiency

--- hackerrank/Sherlock_and_the_Valid_String.cpp
+++ hackerrank/Sherlock_and_the_Valid_String.cpp
@@ -10,12 +10,44 @@ using namespace std;
  */
 
 string isValid(string s) {
-    
-
+    vector<int> v(26, 0);
+    int m = 0;
+    for (auto c : s) {
+        v[c - 'a']++;
+    }
+    map<int, int> n;
+    sort(v.rbegin(), v.rend());
+    for (auto c : v) {
+        if(c == 0)
+            break;
+        n[c]++;
+        m = max(m, n[c]);
+    }
+    for (auto c : n) {
+        if(c.second == m) {
+            m = c.first;
+            break;
+        }
+    }
+    bool ok = true;
+    for(auto c : v) {
+        if (c == 0)
+            break;
+        if (c != m) {
+            if (abs(c - m) > 1 && c > 1)
+                return "NO";
+            else if(abs(c - m) == 1 || c == 1) {
+                if(ok)
+                    ok = false;
+                else 
+                    return "NO";
+            }
+        }
+    }
+    return "YES";
 }
 
-int main()
-{
+int main() {
     ofstream fout(getenv("OUTPUT_PATH"));
 
     string s;
